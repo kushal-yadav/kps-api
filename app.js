30fa9188a83dbfebb019eba550cb9e569d33bc1e
Kushal Yadav <kushaly1996@gmail.com>
2020-01-19 13:12:36 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const axios = require('axios');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 3001
+const organizationID = 649249007;
+const authToken = 'db36e02a50b57e081efe533a8a0f834b';
+
+app.use(cors());
+
+app.get('/contacts', (req, res) => {
+    axios.get('https://books.zoho.com/api/v3/organizations?organization_id=' + organizationID, {
+        headers: {
+            Authorization: authToken
+        }
+    }).then(resp => res.json(resp.data));
+    
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
